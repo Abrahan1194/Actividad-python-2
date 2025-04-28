574a867d130cbc62124a590ce2235021e365de92
Abrahan1194 <abrahan.1194@gmail.com>
2025-04-27 20:23:27 -0500

    Update Verificarnumenlist.py

--- Verificarnumenlist.py
+++ Verificarnumenlist.py
@@ -1,6 +1,5 @@
 numeros= [1, 2, 3, 4, 5]
 numero_buscado = int(input("Ingresa un número: "))
-encontrado = False
 if numero_buscado in numeros:
     print("El número", numero_buscado, "sí está en la lista.")
 else:
